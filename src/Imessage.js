6d5a27a345f7a96734b99c4b12cd3d8dd8993d42
ibe <indra.bayu@bukalapak.com>
2020-10-14 23:34:12 +0700

    create sidebar chats and search template without functionality

---
+++ src/Imessage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Imessage.css';
+import Sidebar from './Sidebar';
+
+function Imessage() {
+  return (
+    <div>
+      <Sidebar />
+
+      {/* Chats */}
+    </div>
+  )
+}
+
+export default Imessage;
